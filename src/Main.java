1bc0da2793a47679b6b2a89dc3554314d2f3df0f
jumaeva1 <meerimzumaeva95@gmail.com>
2023-10-10 19:37:44 +0600

    asdasdasd

---
+++ src/Main.java
@@ -0,0 +1,94 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        /*int [] arr2 = new int [10];
+
+        arr2[0]=67;
+        arr2[1]=69;
+        arr2[3]=89;
+        arr2[4]=35;
+        arr2[5]=38;
+        arr2[6]=100;
+        arr2[7]=350;
+        arr2[8]=356;
+        arr2[9]=21;
+        System.out.println(arr2[0]);
+        System.out.println(arr2[1]);
+        System.out.println(arr2[3]);
+        System.out.println(arr2[4]);
+        System.out.println(arr2[5]);
+        System.out.println(arr2[6]);
+        System.out.println(arr2[7]);
+        System.out.println(arr2[8]);
+        System.out.println(arr2[9]);*/
+
+
+      /*  Random random = new Random();
+
+        int [] array = new int [10];
+
+
+        int count = 0;
+        int sum=0;
+        while (count<10) {
+            array[count] = random.nextInt(1, 20);
+            sum += array.length;
+
+            System.out.println(array[count]);
+            count++;
+
+        }
+
+            System.out.println("среднее"+sum/10);*/
+
+       /* int[] array = {1, 2, 3, 4, 5};
+        int sum = array.length;
+        System.out.println(sum+10);*/
+
+     /*   int [] array = {1,2,3,4,5};
+        int sum = 0;
+
+
+        for (int  i= 0;  i<array.length; i++) {
+            sum += array[i];}
+
+            System.out.println(sum);*/
+
+       /* int []array={1,2,3,4,5};
+        for (int i = array.length; i >0; i--) {
+            System.out.println(i);*/
+     /*   int[] array = {5, 12, 3, 8, 1, 9, 4, 7};
+        int max = 0;
+        int min = 0;
+        max = array[0];
+        min=array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+
+        }
+        System.out.println("макс значение" + max);
+        {
+
+            System.out.println("минимальное значение" + min);*/
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
